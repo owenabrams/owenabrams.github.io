9895d9dd13162a65e62ff595b45753af936b78bc
owenabrams <owenabrams@gmail.com>
2019-07-27 07:19:40 -0700

    Changes today
    Latest Changes

---
+++ portfolio/Vince Tours One Mini/support/_internal/internal.inc.php
@@ -0,0 +1,29 @@
+<?php
+
+/*********************************************************************************
+* LiveZilla internal.inc.php
+* 
+* Copyright 2011 LiveZilla GmbH
+* All rights reserved.
+* LiveZilla is a registered trademark.
+* 
+* Improper changes in this file may cause critical errors.
+* To modify LiveZilla Server settings it is strongly recommended to use 
+* LiveZilla Server Admin application and desist from editing this file directly.
+* 
+********************************************************************************/ 
+
+$INTERN = Array();
+$INTERN["e017d4c"] = Array();
+$INTERN["e017d4c"]["in_id"] = "administrator";
+$INTERN["e017d4c"]["in_level"] = "1";
+$INTERN["e017d4c"]["in_groups"] = "YToxOntpOjA7czoxMjoiVTNWd2NHOXlkQT09Ijt9";
+$INTERN["e017d4c"]["in_name"] = "Anun Abraham";
+$INTERN["e017d4c"]["in_desc"] = "";
+$INTERN["e017d4c"]["in_email"] = "owenabrams@gmail.com";
+$INTERN["e017d4c"]["in_websp"] = 5;
+$INTERN["e017d4c"]["in_perms"] = "2222222";
+$INTERN["e017d4c"]["in_lang"] = "EN";
+$INTERN["e017d4c"]["in_aac"] = "1";
+$INTERN["e017d4c"]["in_lipr"] = "";
+?>
